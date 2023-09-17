15de2c07ebd974b10ca7b8727c6a68cd81a69a3a
999tommy <adegboyetommy@gmail.com>
2023-09-17 16:10:28 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+const sourceJSPattern = /\/src\/.*\.js$/;
+const rollupPlugin = (matchers) => ({
+  name: "js-in-jsx",
+  load(id) {
+    if (matchers.some(matcher => matcher.test(id))) {
+      const file = fs.readFileSync(id, { encoding: "utf-8" });
+      return esbuild.transformSync(file, { loader: "jsx" });
+    }
+  }
+});
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupPlugin([sourceJSPattern])
+      ],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
+  },
+  esbuild: {
+    loader: "jsx",
+    include: [sourceJSPattern],
+    exclude: [],
+  },
+});
